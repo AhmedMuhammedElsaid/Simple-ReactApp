aeb51a332e015070195b630d27f8665ffbcfa1b5
AhmedMuhammedElsaid <ahmed.muhammed.elsaid@gmail.com>
2020-02-01 18:26:26 +0200

    Updates

---
+++ precache-manifest.255467ad4834967a3186d7df1d3664b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5325fa4f496151932014fff713e28499",
+    "url": "/Simple-ReactApp/./index.html"
+  },
+  {
+    "revision": "1f74e0863f7366b63c4a",
+    "url": "/Simple-ReactApp/./static/css/main.1f365914.chunk.css"
+  },
+  {
+    "revision": "f3cb14ca94383112b96c",
+    "url": "/Simple-ReactApp/./static/js/2.fb46653e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Simple-ReactApp/./static/js/2.fb46653e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f74e0863f7366b63c4a",
+    "url": "/Simple-ReactApp/./static/js/main.6d5fae1a.chunk.js"
+  },
+  {
+    "revision": "251cb535bb30707ad84e",
+    "url": "/Simple-ReactApp/./static/js/runtime-main.1401e6b6.js"
+  }
+]);
